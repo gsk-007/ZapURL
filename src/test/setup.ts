bd46d5350f55e410b403089233976b9be95a5b07
gsk-007 <gsingh16742@gmail.com>
2025-03-24 09:32:12 +0530

    fix: sql file name and test db client

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,28 +1,25 @@
 import request from 'supertest'
-import {Client} from 'pg'
 import fs from 'fs'
+import {testClient} from '../config/db'
 
-const client = new Client({
-  connectionString: process.env.DB_URL
-})
 
 // @ts-ignore
 beforeAll(async () => {
-  await client.connect()
-  const createTableSql = fs.readFileSync('create_tables.sql','utf8' )
-  await client.query(createTableSql)
+  await testClient.connect()
+  const createTableSql = fs.readFileSync('create-tables.sql','utf8' )
+  await testClient.query(createTableSql)
 })
 
 // @ts-ignore
 beforeEach(async () => {
-  const tables = (await client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")).rows
+  const tables = (await testClient.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")).rows
   for(const table of tables){
-    await client.query(`TRUNCATE TABLE ${table.table_name} RESTART IDENTITY CASCADE;`)
+    await testClient.query(`TRUNCATE TABLE ${table.table_name} RESTART IDENTITY CASCADE;`)
   }
 })
 
 // @ts-ignore
 afterAll(async () => {
-  await client.end()
+  await testClient.end()
 })
 

929cb4ca5e861b6440ba95385aa4e325351b1b5b
gsk-007 <gsingh16742@gmail.com>
2025-04-15 09:13:03 +0530

    refactor: changes Client to Pool of connections

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,16 @@
-import { Client } from "pg"
+import { Pool} from "pg"
 
-let client:Client
+let client:Pool
 if(process.env.NODE_ENV == 'developement'){
-  client = new Client({
+  client = new Pool({
     connectionString: process.env.DB_URL
   })
 }else if (process.env.NODE_ENV == 'test'){
-  client = new Client({
-    connectionString: process.env.TEST_DB_URL
+  client = new Pool({
+    connectionString: process.env.TEST_DB_URL,
+  })
+  client.on('error', (err) => {
+    console.error('something bad has happened!', err.stack)
   })
 }
 

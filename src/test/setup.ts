1e9a6f96c067c9d1870c1928d98b154f66d6fcc4
gsk-007 <gsingh16742@gmail.com>
2025-04-16 07:47:36 +0530

    refactor: update setup to use pool instead of client

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,25 +1,29 @@
-import request from 'supertest'
-import fs from 'fs'
-import {client} from '../config/db'
-
+import request from "supertest";
+import fs from "fs";
+import { client } from "../config/db";
 
 // @ts-ignore
 beforeAll(async () => {
-  await client.connect()
-  const createTableSql = fs.readFileSync('create-tables.sql','utf8' )
-  await client.query(createTableSql)
-})
+  const createTableSql = fs.readFileSync("create-tables.sql", "utf8");
+  await client.query(createTableSql);
+});
 
 // @ts-ignore
 beforeEach(async () => {
-  const tables = (await client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")).rows
-  for(const table of tables){
-    await client.query(`TRUNCATE TABLE ${table.table_name} RESTART IDENTITY CASCADE;`)
+  
+  const tables = (
+    await client.query(
+      "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'",
+    )
+  ).rows;
+  for (const table of tables) {
+    await client.query(
+      `TRUNCATE TABLE ${table.table_name} RESTART IDENTITY CASCADE;`,
+    );
   }
-})
+});
 
 // @ts-ignore
 afterAll(async () => {
-  await client.end()
-})
-
+  await client.end();
+});

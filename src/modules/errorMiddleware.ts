65865fc6cd359246fea7e9c367e16f9bca800991
gsk-007 <gsingh16742@gmail.com>
2025-06-07 15:38:02 +0530

    feat: add logging with winston refactor: add logger config to middlewares and server

--- src/modules/errorMiddleware.ts
+++ src/modules/errorMiddleware.ts
@@ -1,12 +1,19 @@
-import { Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from "express";
+import logger from "../config/logger";
 
-export const notFound = (req:Request, res: Response, next: NextFunction) => {
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
-}
+};
 
-export const errorHandler = (err:Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  logger.error(`Error: ${err.message}`);
   const statusCode = res.statusCode == 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
@@ -14,4 +21,3 @@ export const errorHandler = (err:Error, req: Request, res: Response, next: NextF
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
-
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,31 @@
-import express  from 'express'
-import "express-async-errors"
-import morgan from 'morgan'
-import router from './router'
-import { errorHandler, notFound } from './modules/errorMiddleware'
-import { limiter } from './config/rateLimiting'
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import router from "./router";
+import { errorHandler, notFound } from "./modules/errorMiddleware";
+import { limiter } from "./config/rateLimiting";
+import logger from "./config/logger";
 
+const app = express();
 
-const app = express()
+app.use((req: Request, _res: Response, next: NextFunction) => {
+  logger.info(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(limiter)
+app.use(limiter);
 
-app.get('/health-check', (req, res) => {
-  res.send("API is running")
-})
+app.get("/health-check", (req, res) => {
+  res.send("API is running");
+});
 
-app.use('/api', router)
+app.use("/api", router);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-export default app
+export default app;

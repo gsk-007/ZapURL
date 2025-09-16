b5468a0eef7e0c7caf3f2f0c82fe3b0e0066f658
gsk-007 <gsingh16742@gmail.com>
2025-09-16 10:12:18 +0530

    fix: husky and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,15 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { ignores: ["**/*.js"] },
+  ...tseslint.configs.recommended,
   { files: ["src/**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
-  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+      ]
+    }
+  },
 ];
